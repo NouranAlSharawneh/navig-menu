8257f8c7dcba92242852937506850de98ea7520e
NouranAlSharawneh <noransharawna@hotmail.com>
2025-01-12 10:45:51 +0300

    Naviagtion Menu

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,113 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+
+import "./menu.css";
+import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const menuLinks = [
+  { label: "Home", path: "/" },
+  { label: "About", path: "/about" },
+  { label: "Work", path: "/work" },
+  { label: "Contact", path: "/contact" },
+  { label: "Lab", path: "/lab" },
+];
+
+export const Menu = () => {
+  const container = useRef();
+  const timeLineAnimation = useRef();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useGSAP(
+    () => {
+      gsap.set(".menu-link-item-holder", {
+        y: 75,
+      });
+
+      timeLineAnimation.current = gsap
+        .timeline({ paused: true })
+        .to(".menu-overlay", {
+          duration: 1.25,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "power4.inOut",
+        })
+        .to(".menu-link-item-holder", {
+          y: 0,
+          duration: 1,
+          stagger: 0.1,
+          ease: "power4.inOut",
+          delay: -0.75,
+        });
+    },
+    { scope: container }
+  );
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      timeLineAnimation.current.play();
+    } else {
+      timeLineAnimation.current.reverse();
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <div className="menu-container" ref={container}>
+      <div className="menu-bar">
+        <div className="menu-logo">
+          <Link href="/">Nunu🍒</Link>
+        </div>
+        <div className="menu-open" onClick={toggleMenu}>
+          <p>Menu</p>
+        </div>
+      </div>
+      <div className="menu-overlay">
+        <div className="menu-overlay-bar">
+          <div className="menu-logo">
+            <Link href="/">Nunu🍒</Link>
+          </div>
+          <div className="menu-close" onClick={toggleMenu}>
+            <p>Close</p>
+          </div>
+        </div>
+        <div className="menu-close-icon">
+          <p>&#x2715;</p>
+        </div>
+        <div className="menu-copy">
+          <div className="menu-links">
+            {menuLinks.map((link, index) => (
+              <div className="menu-link-item" key={index}>
+                <div className="menu-link-item-holder" onClick={toggleMenu}>
+                  <Link href={link.path} className="menu-link">
+                    {link.label}
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="menu-info">
+            <div className="menu-info-col">
+              <a href="#">X &#8599;</a>
+              <a href="#">Instagram &#8599;</a>
+              <a href="#">LinkedIn &#8599;</a>
+              <a href="#">Github &#8599;</a>
+              <a href="#">Dribble &#8599;</a>
+            </div>
+            <div className="menu-info-col">
+              <p>nouran-al-sharawneh@hotmail.com</p>
+              <p>050 7688 769</p>
+            </div>
+          </div>
+        </div>
+        <div className="menu-preview">
+          <p>View Reel</p>
+        </div>
+      </div>
+    </div>
+  );
+};
